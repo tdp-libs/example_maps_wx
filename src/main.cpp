9b6f2721c7e353bbd2a178b9968db94d8aef2896
Tom Paynter <tompaynter@tdpe.co.uk>
2020-02-26 12:51:23 +0000

    Playing with wxWidgets.

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 
 #include "tp_wx_maps/Map.h"
 
+#include "tp_maps/Map.h"
 #include "tp_maps/controllers/FPSController.h"
 #include "tp_maps/layers/GridLayer.h"
 
@@ -12,12 +13,6 @@
 
 #include "tp_utils/DebugUtils.h"
 
-//wx-config --cxxflags --libs
-//#define wxUSE_GUI 1
-#define __WXGTK__
-
-#define _FILE_OFFSET_BITS 64
-#define _LARGE_FILES
 
 #include <wx/wxprec.h>
 #ifndef WX_PRECOMP
@@ -36,12 +31,19 @@ class MyFrame: public wxFrame
 {
 public:
     MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
+
+    ~MyFrame();
 private:
     void OnHello(wxCommandEvent& event);
     void OnExit(wxCommandEvent& event);
     void OnAbout(wxCommandEvent& event);
    // wxDECLARE_EVENT_TABLE();
 };
+
+MyFrame::~MyFrame()
+{
+
+}
 enum
 {
     ID_Hello = 1
@@ -57,26 +59,45 @@ enum
 
 bool MyApp::OnInit()
 {
-    MyFrame *frame = new MyFrame(std::string("Hello World"), wxPoint(50, 50), wxSize(450, 340) );
+    MyFrame *frame = new MyFrame(_T("Hello World"), wxPoint(50, 50), wxSize(450, 340) );
     frame->Show( true );
     return true;
 }
 MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
-        : wxFrame(NULL, wxID_ANY, title, pos, size)
+        : wxFrame(nullptr, wxID_ANY, title, pos, size)
 {
     wxMenu *menuFile = new wxMenu;
-    menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
-                     "Help string shown in status bar for this menu item");
-    menuFile->AppendSeparator();
-    menuFile->Append(wxID_EXIT);
-    wxMenu *menuHelp = new wxMenu;
-    menuHelp->Append(wxID_ABOUT);
+    // menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
+    //                  "Help string shown in status bar for this menu item");
+    // menuFile->AppendSeparator();
+    // menuFile->Append(wxID_EXIT);
+    // wxMenu *menuHelp = new wxMenu;
+    // menuHelp->Append(wxID_ABOUT);
     wxMenuBar *menuBar = new wxMenuBar;
-    menuBar->Append( menuFile, "&File" );
-    menuBar->Append( menuHelp, "&Help" );
+    menuBar->Append( menuFile, _T("&File"));
+    // menuBar->Append( menuHelp, "&Help" );
     SetMenuBar( menuBar );
     CreateStatusBar();
-    SetStatusText( "Welcome to wxWidgets!" );
+    SetStatusText(_T("Welcome to wxWidgets!"));
+
+
+    bool fullScreen = true;
+
+
+    // auto map = new tp_wx_maps::Map(this, true, fullScreen, wxT("TP Maps wxWidgets Example"));
+    //
+    // map->map()->setBackgroundColor({0.0f, 0.0f, 0.0f});
+    //
+    // auto controller = new tp_maps::FPSController(map->map(), fullScreen);
+    // controller->setCameraOrigin({4.0f, 4.0f, 2.0f});
+    // controller->setRotationAngle(270.0f);
+    // TP_UNUSED(controller);
+    //
+    // map->map()->addLayer(new tp_maps::GridLayer());
+    //
+    // example_maps_wx::loadModels(map->map());
+
+
 }
 void MyFrame::OnExit(wxCommandEvent& event)
 {
@@ -84,90 +105,26 @@ void MyFrame::OnExit(wxCommandEvent& event)
 }
 void MyFrame::OnAbout(wxCommandEvent& event)
 {
-    wxMessageBox( "This is a wxWidgets' Hello world sample",
-                  "About Hello World", wxOK | wxICON_INFORMATION );
+    wxMessageBox(_T("This is a wxWidgets' Hello world sample"),
+                  _T("About Hello World"), wxOK | wxICON_INFORMATION );
 }
 void MyFrame::OnHello(wxCommandEvent& event)
 {
-    wxLogMessage("Hello world from wxWidgets!");
+    wxLogMessage(_T("Hello world from wxWidgets!"));
 }
 
+IMPLEMENT_APP_NO_MAIN(MyApp)
 
 
 
 
 //##################################################################################################
-int main()
+int main(int argc, char **argv)
 {
   tp_utils_filesystem::init();
   tp_image_utils_freeimage::init();
 
-  bool fullScreen = true;
 
-  tp_wx_maps::Map map(true, fullScreen, "TP Maps wxWidgets Example");
-  map.setBackgroundColor({0.0f, 0.0f, 0.0f});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  auto controller = new tp_maps::FPSController(&map, fullScreen);
-  controller->setCameraOrigin({4.0f, 4.0f, 2.0f});
-  controller->setRotationAngle(270.0f);
-  TP_UNUSED(controller);
-
-  map.addLayer(new tp_maps::GridLayer());
-
-  example_maps_wx::loadModels(&map);
-
-  map.exec();
-  return 0;
+  //map.exec();
+  return wxEntry(argc, argv);
 }
